dddd66112b74adf0f391e15b26bc88e304d44fb8
Aidar4ik <aidaroffical@mail.ru>
2023-08-25 17:54:54 +0600

    Initial commit

---
+++ app/src/main/java/com/example/lesson_2_3/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.lesson_2_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().add(R.id.Frame, new FirstFragment()).commit();
+
+        }
+    }
+}
